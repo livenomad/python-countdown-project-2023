2ef078a41982dee8c50e8cd44a7aa45779712bb5
Livenomad <Macklinthomas5@gmail.com>
2023-07-01 12:35:47 -0500

    Add files via upload

---
+++ countdown_calendar.py
@@ -0,0 +1,46 @@
+from tkinter import Tk, Canvas
+from datetime import date, datetime
+
+def get_events():
+    list_events = []
+    with open('events.txt') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            current_event = line.split(',')
+            event_date = datetime.strptime(current_event[1], '%m/%d/%Y').date()
+            current_event[1] = event_date
+            list_events.append(current_event)
+    return list_events
+
+def days_between_dates(date1, date2):
+    time_between = str(date1-date2)
+    number_of_days = time_between.split(' ')
+    return number_of_days[0]
+
+
+
+root = Tk()
+c = Canvas(root, width=800, height=800, bg='black')
+c.pack()
+c.create_text(100, 50, anchor='w', fill='green', font='Courier 38 bold underline', text='My Countdown Calendar')
+events = get_events()
+today = date.today()
+
+vertical_spacing = 100
+
+for event in events:
+    event_name = event[0]
+    days_until = days_between_dates(event[1], today)
+    display = 'It is %s days until %s' % (days_until, event_name)
+    if (int(days_until) <= 7):
+        text_col = 'red'
+    else:
+        text_col = 'lightblue'
+    c.create_text(100, vertical_spacing, anchor='w', fill = text_col, font='Arial 28 bold', text=display)
+
+    vertical_spacing = vertical_spacing + 30
+
+
+root.mainloop()
+
+
